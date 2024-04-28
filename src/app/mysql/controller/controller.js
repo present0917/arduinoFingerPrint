33e9450248897a400d8f08e590fd2d3ce03a2041
present0917 <guswo0329@gmail.com>
2024-04-28 13:13:47 +0900

    first take

---
+++ src/app/mysql/controller/controller.js
@@ -0,0 +1,138 @@
+const db = require('../model/index.js');
+const Tutorial = db.tutorial;
+const Op = db.sequelize.Op;
+
+// Create tutorial
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+        res.status(400).send({
+            message: 'Title is empty!'
+        });
+        return;
+    }
+
+    // Set tutorial
+    const tutorial = {
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    };
+
+    // Save tutorial
+    Tutorial
+        .create(tutorial)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Create tutorial failure.'
+            });
+        });
+};
+
+// Retrieve all tutorials
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+    let condition = { where: {} };
+    let keyword;
+    if (keyword) {
+        condition = {
+            where : {
+                [Op.or]: [
+                    {
+                        title: {
+                            [Op.like]: `%${keyword}%`
+                        }
+                    },
+                    {
+                        description: {
+                            [Op.like]: `%${keyword}%`
+                        }
+                    }
+                ]
+            }
+        }
+    };
+
+    Tutorial
+        .findAll(condition)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Retrieve all tutorials failure.'
+            });
+        });
+};
+
+// Retrieve tutorial by id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Tutorial
+        .findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Retrieve tutorial failure. (id: ' + id + ')'
+            });
+        });
+};
+
+// Update tutorial by id
+exports.update = (req, res) => {
+    const id = req.params.id;
+    const condition = id ? { where: { id: id } } : null;
+
+    Tutorial
+        .update(
+            req.body,
+            condition
+        )
+        .then(resultCount => {
+            if (resultCount == 1) {
+                res.send({
+                    message: 'Tutorial updated.'
+                });
+            } else {
+                res.send({
+                    message: 'Cannot update tutorial. (id: ' + id + ')'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Update tutorial failure. (id: ' + id + ')'
+            });
+        });
+};
+
+// Delete tutorial by id
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    const condition = id ? { where: { id: id } } : null;
+
+    Tutorial
+        .destroy(condition)
+        .then(resultCount => {
+            if (resultCount == 1) {
+                res.send({
+                    message: 'Tutorial deleted.'
+                });
+            } else {
+                res.send({
+                    message: 'Cannot delete tutorial. (id: ' + id + ')'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Delete tutorial failure. (id: ' + id + ')'
+            });
+        });
+};
---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Set CORS option
+app.use(cors());
+
+// Parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// Parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// RESTful API route for DB
+app.use('/', require('./app/mysql/route/route.js'));
+
+// DB Connection
+const db = require('./app/mysql/model/index.js');
+      db.sequelizeConfig.sync();
+
+// Default route for server status
+app.get('/', (req, res) => {
+  res.json({ message: `Server is running on port ${PORT}` });
+});
+
+// Set listen port for request
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
